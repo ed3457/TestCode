dd1c8d448e59205078e56a54e6aee02e5495b7ef
Kamel R <kamel.r@wayne.edu>
2024-05-14 18:50:57 -0400

    created objects

--- TestCode.cpp
+++ TestCode.cpp
@@ -6,9 +6,24 @@
 using namespace std; 
 int main()
 {
-    cout << "Hello World!\n";
+    //cout << "Hello World!\n";
 
-    // 
+    // create a number of objects 
+
+    Student student1; // object of type: Student 
+    Student student2;
+
+    student1.setAccessID("AB1234");
+    student1.setGPA(3.0);
+    student1.setMajor("English");
+    student1.setName("Steve Jobs");
+
+    student1.printStudentInfo();
+
+    cout << "----------------------------\n";
+    
+    student2.printStudentInfo();
+
 }
 
 
