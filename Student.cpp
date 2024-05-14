78622dc246429a139015f41b40e7bbf4a9f59a3f
Kamel R <kamel.r@wayne.edu>
2024-05-14 18:21:35 -0400

    finished the set/get for all fields

--- Student.cpp
+++ Student.cpp
@@ -4,7 +4,6 @@ void Student::setName(string n)
 {   // usually, we do some validation here 
 	name = n;
 
-
 }
 
 string Student::getName()
@@ -12,3 +11,37 @@ string Student::getName()
 	return name; 
 
 }
+
+void Student::setMajor(string m)
+{
+	major = m;
+
+}
+
+string Student::getMajor()
+{
+	return major;
+
+}
+
+void Student::setAccessID(string a)
+{
+	accessId = a;
+}
+
+string Student::getAccessID()
+{
+	return accessId;
+}
+
+void Student::setGPA(float g)
+{
+	gpa = g;
+
+}
+
+float Student::getGPA()
+{
+	return gpa;
+}
+
